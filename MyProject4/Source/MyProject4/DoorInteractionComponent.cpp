6b526adfabf1b9813c8b3af30c2f8772a4b27fff
Alejandro <cedillito7@gmail.com>
2023-01-31 16:30:28 +0000

    Interaction with door
    interactable door, has a trigger box, with a open curve.
    Has a BP so that it can be used and change values to have different doors.

---
+++ MyProject4/Source/MyProject4/DoorInteractionComponent.cpp
@@ -0,0 +1,78 @@
+#include "DoorInteractionComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/TriggerBox.h"
+#include "Engine/World.h"
+
+#include "DrawDebugHelpers.h"
+
+constexpr float FLT_METERS(float meters) { return meters * 100.0f; }
+
+static TAutoConsoleVariable<bool> CVarToggleDebugDoor(TEXT("DoorInteractableComponent.Debug"), false, TEXT("DoorInteractableComponent debug Display."), ECVF_Default);
+
+// Sets default values for this component's properties
+UDoorInteractionComponent::UDoorInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	DoorState = EDoorState::DS_Closed;
+
+	CVarToggleDebugDoor.AsVariable()->SetOnChangedCallback(FConsoleVariableDelegate::CreateStatic(&UDoorInteractionComponent::OnDebugToggle));
+
+	// ...
+}
+
+void UDoorInteractionComponent::OnDebugToggle(IConsoleVariable* Var)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnDebugToggled"));
+}
+
+void UDoorInteractionComponent::DebugDraw()
+{
+	if (CVarToggleDebugDoor->GetBool())
+	{
+		FVector Offset(FLT_METERS(-0.75f), 0.0f, FLT_METERS(2.5f));
+		FVector StartLocation = GetOwner()->GetActorLocation() + Offset;
+		FString EnumAsString = TEXT("DoorState: ") + UEnum::GetDisplayValueAsText(DoorState).ToString();
+		DrawDebugString(GetWorld(), Offset, EnumAsString, GetOwner(), FColor::Blue, 0.0f);
+	}
+}
+
+
+// Called when the game starts
+void UDoorInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	StartRotation = GetOwner()->GetActorRotation();
+	FinalRotation = GetOwner()->GetActorRotation() + DesiredRotation;
+
+	CurrentRotationTime = 0.0f;
+	// ...
+
+}
+
+
+// Called every frame
+void UDoorInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (CurrentRotationTime < TimeToRotate)
+	{
+		if (TriggerBox && GetWorld() && GetWorld()->GetFirstLocalPlayerFromController())
+		{
+			APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+			if (PlayerPawn && TriggerBox->IsOverlappingActor(PlayerPawn))
+			{
+				CurrentRotationTime += DeltaTime;
+				const float TimeRatio = FMath::Clamp(CurrentRotationTime / TimeToRotate, 0.0f, 1.0f);
+				const float RotationAlpha = OpenCurve.GetRichCurveConst()->Eval(TimeRatio);
+				const FRotator CurrentRotation = FMath::Lerp(StartRotation, FinalRotation, RotationAlpha);
+				GetOwner()->SetActorRotation(CurrentRotation);
+			}
+		}
+	}
+	// ...
+}
