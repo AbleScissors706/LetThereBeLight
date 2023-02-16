5e3745a98ebe75479ed9e66d8524d5fc4a462ced
Alejandro <cedillito7@gmail.com>
2023-02-16 13:58:06 +0000

    implementing a pickup

---
+++ LetThereBeLight/Source/MyProject4/PickUp.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickUp.h"
+
+// Sets default values
+APickUp::APickUp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	//all pickups start active
+	bIsActive = true;
+
+	//create staticmeshcomponent
+	PickUpMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickUpMesh"));
+	RootComponent = PickUpMesh;
+}
+
+// Called when the game starts or when spawned
+void APickUp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickUp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
